e946d2e2990f5ca93b94efb05e9f270b8624ee1e
michael clark <michaelclark@michaels-Air.fios-router.home>
2022-03-18 11:44:31 -0400

    configured babel file so some dependencies can run correctly

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native'
 
 const SignupScreen = () => {
-    return <Text style={{  fontSize: 48  }}>AccountScreen </Text>
+    return <Text style={{  fontSize: 48  }}>SignupScreen </Text>
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native'
 
 const TrackCreateScreen = () => {
-    return <Text style={{  fontSize: 48  }}>AccountScreen </Text>
+    return <Text style={{  fontSize: 48  }}>TrackCreateScreen </Text>
 };
 
 const styles = StyleSheet.create({});
