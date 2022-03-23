5efedf079ac2bad86178c386fa17665a9f98861a
michael clark <michaelclark@michaels-Air.fios-router.home>
2022-03-23 12:18:59 -0400

    added axios to handle components interacting with server data.

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'http://6e93-71-167-6-6.ngrok.io'
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,23 @@
 import createDataContext from "./createDataContext";
+import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };
         default:
             return state;
     }
 };
 
 const signup = (dispatch) => {
-    return ({ email, password }) => {
-
+    return async ({ email, password }) => {
+        try{
+            const response = await trackerApi.post('/signup', { email, password });
+            console.log(response.data);
+        } catch (err) {
+            dispatch({ type: 'add_error', payload: 'Something went wrong while signing up.' });
+        }
     };
 };
 
@@ -28,5 +36,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signin, signout, signup },
-    { isSignedIn: false } 
+    { isSignedIn: false, errorMessage: '' } 
 );
