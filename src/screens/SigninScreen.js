690bb8fe6014774fe1cc317660932707c26a788d
michael clark <michaelclark@michaels-Air.fios-router.home>
2022-03-24 11:31:17 -0400

    refactored to make use of components for sign in/ sign up screens

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-    return <Text style={{  fontSize: 48  }}>SigninScreen </Text>
+    return (
+        <View style={styles.container}>
+            <AuthForm 
+                headerText='Sign in to Your Account'
+                errorMessage=''
+                onsubmit={() => {}}
+                submitButtonText='Sign In'
+            />
+            <NavLink 
+                text='No account? Signup instead!'
+                routeName='Signup'
+            />
+        </View>
+    )
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+    return {
+      header: () => false,
+    };
+  };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
+});
 
 export default SigninScreen;
