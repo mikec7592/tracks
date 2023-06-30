0e437f5d791a6250f825fa8847c8849cc1b1d2ad
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-06-30 14:14:57 -0700

    Commit before sdk upgrade attempt.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://6e93-71-167-6-6.ngrok.io'
+    baseURL: ' http://5383-2601-600-807f-e240-8926-9fb6-e797-584c.ngrok-free.app'
 });
+
+// baseURL: 'http://6e93-71-167-6-6.ngrok.io'
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -8,7 +8,7 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload };
-            case 'signup':
+            case 'signin':
                 return{ errorMessage: '', token: action.payload };
         default:
             return state;
@@ -20,7 +20,7 @@ const signup = (dispatch) => {
         try{
             const response = await trackerApi.post('/signup', { email, password });
             await AsyncStorage.setItem('token', response.data.token);
-            dispatch({ type: 'signup', payload: response.data.token});
+            dispatch({ type: 'signin', payload: response.data.token});
 
             navigate('TrackList');
         } catch (err) {
@@ -31,11 +31,20 @@ const signup = (dispatch) => {
     };
 };
 
-const signin = (dispatch) => {
-    return ({ email, password }) => {
-
+const signin = (dispatch) => 
+    async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signin', { email, password });
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({ type: 'signin', payload: response.data.token });
+            navigate('TrackList')
+        } catch (err) {
+            dispatch({
+                type: 'add_error',
+                payload: 'Something went wrong with sign in'
+            });
+        }
     };
-};
 
 const signout = (dispatch) => {
     return () => {
